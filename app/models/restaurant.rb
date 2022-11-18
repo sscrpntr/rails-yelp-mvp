b248579cd806151aead0b610a154ce7c4fa14eb6
Sergi Escarpenter <sergiescarpenter@gmail.com>
2022-11-18 15:51:00 +0100

    needed help on the path to restaurants once creating a review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :name, :address, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
+  has_many :reviews, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,4 @@ Rails.application.routes.draw do
     # Defines the root path route ("/")
     # root "articles#index"
   end
-  resources :reviews, only: [:destroy]
 end
